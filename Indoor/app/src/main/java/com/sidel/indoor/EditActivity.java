83f797882fe834ee0c282e4b336d4c973faa348d
luside <lusd1994@gmail.com>
2017-10-06 21:42:19 +1100

    mainactivity

---
+++ Indoor/app/src/main/java/com/sidel/indoor/EditActivity.java
@@ -0,0 +1,8 @@
+package com.sidel.indoor;
+
+/**
+ * Created by lsd20 on 06/10/2017.
+ */
+
+public class EditActivity extends MainActivity {
+}
--- Indoor/app/src/main/java/com/sidel/indoor/MainActivity.java
+++ Indoor/app/src/main/java/com/sidel/indoor/MainActivity.java
@@ -1,17 +1,59 @@
 package com.sidel.indoor;
 
 import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.wifi.ScanResult;
+import android.net.wifi.WifiManager;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import java.util.List;
+
+public class MainActivity extends Activity implements View.OnTouchListener{
+
+    protected Map map;
+    protected WifiManager wifiManager;
+    protected BroadcastReceiver broadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wifiManager = (WifiManager)getApplicationContext().getSystemService(Context.WIFI_SERVICE);
+        map = (Map)findViewById(R.id.map);
+        map.setOnTouchListener(this);
+    }
+
+    protected void onStart(){
+        super.onStart();
+        broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                List<ScanResult> scanresults= wifiManager.getScanResults();
+                receiveScanResults(scanresults);
+            }
+        };
+        registerReceiver(broadcastReceiver, new IntentFilter(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION));
+    }
+
+    protected void receiveScanResults(List<ScanResult> scanResults){
+
+    }
+
+    protected void onStop(){
+        super.onStop();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    public boolean onTouch(View view, MotionEvent event){
+        view.onTouchEvent(event);
+        return true;
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -24,6 +66,7 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case 1:
+                startEditActivity();
                 return true;
             case 2:
                 startCompassActivity();
@@ -37,6 +80,11 @@ public class MainActivity extends Activity {
 
     public void startCompassActivity(){
         Intent intent = new Intent(MainActivity.this, CompassActivity.class);
+        startActivity(intent);
+    }
+
+    public void startEditActivity(){
+        Intent intent = new Intent(MainActivity.this, EditActivity.class);
         startActivity(intent);
     }
 }
